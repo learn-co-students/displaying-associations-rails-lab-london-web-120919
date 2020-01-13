b9a316a4adfc61455fbb65764cbc803f72ff5ea3
flatironschool <t.benson@live.co.uk>
2020-01-13 18:48:43 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+
+    def song_count
+        songs = Song.all.select{ |song| song.artist_id == self.id }
+        self.songs.count
+    end
 end
