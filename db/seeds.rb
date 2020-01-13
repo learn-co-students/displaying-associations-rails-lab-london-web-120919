99a17461c5caf0680cbbadc55d77dbfdb11c6bd1
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2020-01-13 17:59:38 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.create!(name: "Motivation")
+a1.songs.create!(title: "10 Ways You Are Already Awesome")
+a1.songs.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+a1.songs.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
+a2 = Artist.create!(name: "Movies")
+a2.songs.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
+
